7fafb8865b2d3ccab922334de2e1b96391f278f2
danrigoni <danrigonisocial@gmail.com>
2020-10-03 17:41:06 -0300

    initial rust

---
+++ calc/target/rls/debug/deps/calc-681e5cb797bf68a3.d
@@ -0,0 +1,5 @@
+/home/danrigoni/code/studies/rust/calc/target/rls/debug/deps/calc-681e5cb797bf68a3.rmeta: src/main.rs
+
+/home/danrigoni/code/studies/rust/calc/target/rls/debug/deps/calc-681e5cb797bf68a3.d: src/main.rs
+
+src/main.rs:
---
+++ calc/target/rls/debug/deps/calc-c4edc553d6b7f19f.d
@@ -0,0 +1,5 @@
+/home/danrigoni/code/studies/rust/calc/target/rls/debug/deps/calc-c4edc553d6b7f19f.rmeta: src/main.rs
+
+/home/danrigoni/code/studies/rust/calc/target/rls/debug/deps/calc-c4edc553d6b7f19f.d: src/main.rs
+
+src/main.rs:
